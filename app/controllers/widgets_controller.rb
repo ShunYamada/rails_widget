ae9187938391cb299fbb94ee9ae2444aedfb9cb5
yamada <sy@gociao.com>
2018-09-10 13:38:58 -0400

    second commit

---
+++ app/controllers/widgets_controller.rb
@@ -0,0 +1,14 @@
+class WidgetsController < ApplicationController
+  def show
+    respond_to do |format|
+      format.html { render params[:template], layout: 'widgets' }
+      format.js   { render js: js_constructor }
+    end
+  end
+
+  private
+  def js_constructor
+    content = render_to_string(params[:template], layout: false)
+    "document.write(#{content.to_json})"
+  end  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'pages/index'
+  get '/widgets/:template', to: 'widgets#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
